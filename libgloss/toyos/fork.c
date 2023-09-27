f53a226587f2cf417f07453c38b43a105fce484c
tangruiwen.mmh1103 <tangruiwen.mmh1103@bytedance.com>
2023-09-27 16:57:27 +0800

    implement fork syscall

--- libgloss/toyos/fork.c
+++ libgloss/toyos/fork.c
@@ -2,18 +2,17 @@
 #include <errno.h>
 
 #include "config.h"
+#include "syscall.h"
 
 #undef errno
 
 extern int errno;
 
 int fork() {
-  int ret;
+  int ret = toyos_syscall0(__NR_fork);
 
-  /* create a child process */
-  ret = -1;
-  if (ret < 0) {
-    errno = -ret;
+  if (ret == -1) {
+    errno = ENOSYS;
     ret = -1;
   }
 
